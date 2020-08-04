01edf944b5f0a14d624b39805466f6ef49bc09e9
CDRMV <46399086+CDRMV@users.noreply.github.com>
2020-08-04 20:32:32 +0200

    Upload Units folder and general files

---
+++ mod_info.lua
@@ -0,0 +1,17 @@
+name = "(F.B.P.) Future Battlefield Pack: Infantry Warfare"
+uid = "5t3edt-btz6-9438-h6ui-967gt56fa9"
+version = 1.0
+copyright = "Copyright © 2020, CDRMV (aka CDR Deadalus)"
+description = "'Future Battlefield Pack: Infantry Warfare' is a Submod for F.B.P: Legends, which adds Infantry Classes into the Game."
+author = "CDRMV (aka CDR Deadalus)"
+url = "http://www.moddb.com/mods/fbp-future-battlefield-pack"
+icon = "/mods/Future Battlefield Pack Infantry Warfare/Icon.png" 
+selectable = true
+enabled = true
+exclusive = false
+ui_only = false
+requires = { }
+requiresNames = { }
+conflicts = { }
+before = { }
+after = { }
